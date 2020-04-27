7458f52caf5c2e86bb54dcd16d8bb68430673025
Natan Z <natan.zaizev@gmail.com>
2020-04-27 18:18:59 +0300

    added success and failure pages after submitting

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-
   res.sendFile(__dirname + "/signup.html");
 });
 
@@ -40,18 +39,22 @@ app.post("/", function (req, res) {
   }
   const request = https.request(url, options, function (response) {
     response.on("data", function (data) {
+      
+      if(response.statusCode == 200){
+        res.sendFile(__dirname + "/success.html");
+      }
+
+      else{
+        res.sendFile(__dirname + "/failure.html" )
+      }
       console.log(JSON.parse(data));
     });
-
   });
   request.write(jsonData);
   request.end();
-
 })
 
-
 app.listen(3000, function () {
-
   console.log("Server is running on port - 3000");
 });
 
